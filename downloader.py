d97bcbc1175b5d357d8117e2cfd05e535421f19a
Asier <asierserrano8@gmail.com>
2024-08-17 19:41:21 +0200

    Added option to select stream

--- downloader.py
+++ downloader.py
@@ -1,50 +1,40 @@
 import re
-from time import time
 from pytubefix import YouTube
 from colorama import Fore, Style
+from math import trunc
 
+def is_valid_url(url):
+    # Regular expression to check YouTube URL format
+    pattern = r'(https?://)?(www\.)?(youtube)\.(com|be)/.+'
+    # Check if the URL matches the pattern
+    return re.match(pattern, url) is not None
 
-def es_url_valida(url):
-    # Expresión regular para verificar el formato de la URL de YouTube
-    patron = r' ?(https?://)?(www\.)?(youtube)\.(com)/.+'
-    # Comprobar si la URL coincide con el patrón
-    if re.match(patron, url):
-        return True
-    else:
-        return False
-
-def descargar_video(stream,folder):
+def download_video(stream, folder):
     try:
         stream.download(folder)
-        print(f"{Fore.GREEN}Descarga completada exitosamente.{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}Download completed successfully.{Style.RESET_ALL}")
     except Exception as e:
-        print(f"{Fore.RED}Error durante la descarga: {e}{Style.RESET_ALL}")
+        print(f"{Fore.RED}Error during download: {e}{Style.RESET_ALL}")
 
 if __name__ == "__main__":
-    print("--Descargador de videos de youtube--")
-    url = input("Introduzca la url del video a descargar (ejemplo: https://www.youtube.com/watch?v=XXXXXXXXXXX):")
-
-    if es_url_valida(url):
-        print(f"{Fore.GREEN}La URL es válida.{Style.RESET_ALL}")
+    url = input("Enter the YouTube URL: ")
+    if is_valid_url(url):
+        print(f"{Fore.GREEN}The URL is valid.{Style.RESET_ALL}")
         yt = YouTube(url)
-        print(f"Titulo: {Fore.CYAN}{yt.title}{Style.RESET_ALL}")
-        print(f"Creador/a: {Fore.CYAN}{yt.author}{Style.RESET_ALL}")
-        streams = yt.streams.filter(file_extension="mp4", progressive=True)
-        
-        # Seleccionar el primer stream disponible
-        stream = streams.first()
-        if stream:
-            print(f"Resolución: {stream.resolution}")
-            print(f"Tipo de archivo: {stream.mime_type}")
-            print(f"Tamaño del archivo: {stream.filesize} bytes")             
-            folder = input("Donde quieres almacenar el video: ")
-            
-            time_initial = time()
-            descargar_video(stream,folder)
-            time_finish = time()
-            total_time = time_finish - time_initial
-            print(f"Tiempo tardado: {total_time}s")
+        print(f"Title: {Fore.CYAN}{yt.title}{Style.RESET_ALL}")
+        print(f"Creator: {Fore.CYAN}{yt.author}{Style.RESET_ALL}")
+
+        for index, stream in enumerate(yt.streams):
+            print(f"[{index}]\t File type: {stream.mime_type}")
+            print(f"\t Audio codec: {stream.codecs}" if stream.includes_audio_track else f"\t Resolution: {stream.resolution}")
+            print(f"\t File size: {trunc(stream.filesize / (1024*1024))} MB")
+            print(f"\t Progressive: {stream.is_progressive}\n")
+
+        stream_input = int(input("Select the stream to download: "))
+        if stream_input >= 0 and stream_input < len(yt.streams):
+            folder = input("Enter the folder: ")
+            download_video(list(yt.streams)[stream_input], folder)
         else:
-            print(f"{Fore.RED}No se encontraron streams adecuados para descargar.{Style.RESET_ALL}")
+            print(f"{Fore.RED}No suitable streams found for download.{Style.RESET_ALL}")
     else:
-        print(f"{Fore.RED}La URL no es válida.{Style.RESET_ALL}")
+        print(f"{Fore.RED}The URL is not valid.{Style.RESET_ALL}")
